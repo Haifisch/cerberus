26e06cdd061039a41a530d5a90a5bc966bd8c618
haifisch <laws.ra1n@me.com>
2016-11-06 02:13:20 -0800

    Initial Commit

---
+++ Cerberus/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cerberus
+//
+//  Created by haifisch on 11/6/16.
+//  Copyright © 2016 haifisch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
